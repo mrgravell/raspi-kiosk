4e03335c1eb8ac1d0b0da5c3eff5454d6f2a0c02
Peter Gravell <peter.gravell@gmail.com>
2017-11-17 20:43:33 +0000

    rewrote to fix bugs and show images

--- cron_script.sh
+++ cron_script.sh
@@ -1,64 +1,96 @@
 #!/bin/bash
 
-# we'll need this at some point
-#
-# feh --hide-pointer -x -q -D 5 -B black -g 320x240 /path/to/pictures
-#
-
 #require a screen
 export DISPLAY=:0
 
 #blank the screen
+echo "s activate"
 xset s activate
 
-cd /home/pi/kiosk
-#delete bs dot files and system volume information
-#rm /home/pi/kiosk/usb_clone/.??*
-rm -r /mnt/usb/System\ Volume\ Information
-rm -r /mnt/usb/.??*
+#nb 2 seconds get added to this
+IMG_TIME='6'
+
+THIS_PATH='/home/pi/kiosk'
+USB_PATH='/mnt/usb'
+CLONE_PATH='/home/pi/kiosk/usb_clone'
+
+#begin infinite loop
+while true
+do
+
 #check if usb contains files
-files=$(shopt -s nullglob dotglob; echo /mnt/usb/*)
+files=$(shopt -s nullglob dotglob; echo ${USB_PATH}/*)
 if (( ${#files} ))
 then
   echo "contains files"
+
+  #hide screensaver and show loadings image
+  echo "s reset"
+  xset s reset
+  feh --hide-pointer -x ${THIS_PATH}/loading.jpg &
+
+  #clear out crap from usb so we don't have to copy it
+  echo "deleting crap from usb"
+  rm -r ${USB_PATH}/System\ Volume\ Information
+  rm -r ${USB_PATH}/.??*
+
   #check for diff
-  OUTPUT1="$(diff -r /mnt/usb/ /home/pi/kiosk/usb_clone/ | grep /mnt/usb/ | awk '{print $4}')"
-  OUTPUT2="$(diff -r /home/pi/kiosk/usb_clone/ /mnt/usb/ | grep /home/pi/kiosk/usb_clone/ | awk '{print $4}')"
+  echo "check for differences ignoring .gitignore"
+  OUTPUT1="$(diff -r -x '.gitignore' ${USB_PATH}/ ${CLONE_PATH}/ | grep ${USB_PATH}/ | awk '{print $4}')"
+  OUTPUT2="$(diff -r -x '.gitignore' ${CLONE_PATH}/ ${USB_PATH}/ | grep ${CLONE_PATH}/ | awk '{print $4}')"
   OUTPUT=${OUTPUT1}${OUTPUT2}
   if [[ -z "${OUTPUT// }" ]] || [[ -z "${OUTPUT// }" ]]
   then
-    echo "no changes"
-
-  SC_CODE="$(pidof play_script.sh | wc -w)"
-  OM_CODE="$(pidof omxplayer.bin | wc -w)"
-  CODE=$((${SC_CODE}+${OM_CODE}))
-  echo "code is ${CODE}"
-case ${CODE} in
-
-0)  echo "Starting Script:"
-    ./play_script.sh
-    ;;
-1)  echo "play_script.sh already running"
-    ;;
-esac
-
+    echo "no differences"
   else
-    #first stop the play script if it is running
-    echo "trying to kill play_script.sh and omxplayer"
-    pkill feh
-    pkill omxplayer.bin
-    pkill play_script.sh
-    echo "disable screensaver, show waiting image and continue execution"
-    xset s reset
-    feh --hide-pointer -x /home/pi/kiosk/loading.jpg &
+    echo "got differences"
     #sync the local directory to the usb. delete local files if deleted from usb.
-    rsync -r -v --exclude '.gitignore' --delete /mnt/usb/ /home/pi/kiosk/usb_clone
-    #now start the play script
-    echo "enable screensaver, start play script"
-    xset s activate
-    pkill feh
-    ./play_script.sh
+    echo "syncing usb to local folder"
+    rsync -r -v --exclude '.gitignore' --delete ${USB_PATH}/ ${CLONE_PATH}
   fi
+
+  #unmount the usb - no longer needed
+  echo "unmounting usb"
+  umount ${USB_PATH}
+
+  #reactivate screensaver & kill loading image
+  echo "s activate"
+  xset s activate
+  pkill feh
+
 else
   echo "usb empty or unplugged"
 fi
+
+#now loop thorugh each
+echo "enter play loop now"
+FILES=${CLONE_PATH}/*
+for f in $FILES
+do
+  #get extrnsion
+  ext="${f##*.}"
+  #convert to lowercase
+  ext="${ext,,}"
+  echo $ext
+if [[ $ext = 'mov' || $ext = 'mp4' ]]
+then
+ echo "trying to play"
+ omxplayer $f
+fi
+if [[ $ext = 'jpg' || $ext = 'jpeg' || $ext = 'png' ]]
+then
+  echo "trying to show image"
+  feh --hide-pointer --full-screen $f &
+  sleep 1
+  echo "s reset"
+  xset s reset
+  sleep $IMG_TIME
+  echo "s activate"
+  xset s activate
+  sleep 1
+  pkill feh
+fi
+done
+echo "end of loop"
+#end infinite loop
+done
